d84d5234529109549f91782d8ddcda86c5fdac94
Michele Schimd <michele.schimd@itiszuccante.edu.it>
2022-06-30 13:34:07 +0200

    Revert of classes to funtions.

--- src/components/FillQuestion.js
+++ src/components/FillQuestion.js
@@ -1,35 +1,34 @@
 import React from "react";
 import QuestionInfoPanel from "./QuestionInfoPanel";
 
-class FillQuestion extends React.Component {
+function FillQuestion(props) {
 
-    render() {
-        var text = this.props.text;
-        for (var i = 0; i < this.props.correct.length; i++) {
-            var searchPattern = `{{${i}}}`;
-            var inputHTML = `<input type="text" id="ans-${this.props.id}-${i}" aria-label="input risposta">`
-            text = text.replace(searchPattern, inputHTML);
-        }
-        var difficultyText = <span className="badge bg-success">Facile</span>;
-        if (this.props.weight > 2) {
-            difficultyText = (this.props.weight > 4) ?
-                <span className="badge bg-warning">Media</span> :
-                <span className="badge bg-danger">Difficile</span>;
-        }
-        return (
-            <div id={`question-${this.props.id}`} className="mx-3 my-2 py-2 px-4 border">
-                <div className="py-1">
-                    <strong>Domanda {this.props.questionNumber}</strong>
-                    <br />{difficultyText}
-                    <div dangerouslySetInnerHTML={{ __html: text}} />
-                </div>
-                <QuestionInfoPanel
-                    maxPoints={this.props.weight}
-                    score={null}
-                />
-            </div>
-        );
+    var text = props.text;
+    for (var i = 0; i < props.correct.length; i++) {
+        var searchPattern = `{{${i}}}`;
+        var inputHTML = `<input type="text" id="ans-${props.id}-${i}" aria-label="input risposta">`
+        text = text.replace(searchPattern, inputHTML);
     }
+    var difficultyText = <span className="badge bg-success">Facile</span>;
+    if (props.weight > 2) {
+        difficultyText = (props.weight > 4) ?
+            <span className="badge bg-warning">Media</span> :
+            <span className="badge bg-danger">Difficile</span>;
+    }
+    return (
+        <div id={`question-${props.id}`} className="mx-3 my-2 py-2 px-4 border">
+            <div className="py-1">
+                <strong>Domanda {props.questionNumber}</strong>
+                <br />{difficultyText}
+                <div dangerouslySetInnerHTML={{ __html: text }} />
+            </div>
+            <QuestionInfoPanel
+                maxPoints={props.weight}
+                score={null}
+            />
+        </div>
+    );
+
 }
 
 export default FillQuestion;
--- src/components/SingleQuestion.js
+++ src/components/SingleQuestion.js
@@ -2,45 +2,44 @@ import React from "react";
 import shuffle from "../util";
 import QuestionInfoPanel from "./QuestionInfoPanel";
 
-class SingleQuestion extends React.Component {
+function SingleQuestion(props) {
 
-    render() {
-        const options = shuffle(this.props.options.map((option, index) => (
-            <div key={`${this.props.id}-${index}`} className="form-check">
-                <input
-                    className="form-check-input"
-                    type="radio"
-                    id={`ans-${this.props.id}-${index}`}
-                    name={`q-${this.props.id}-${index}`} />
-                <label className="form-check-label">
-                    <div dangerouslySetInnerHTML={{ __html: option }} />
-                </label>
-            </div>
-        )));
-        var difficultyText = <span className="badge bg-success">Facile</span>;
-        if (this.props.weight > 2) {
-            difficultyText = (this.props.weight > 4) ?
-                <span className="badge bg-warning">Media</span> :
-                <span className="badge bg-danger">Difficile</span>;
-        }
-        return (
-
-            <div id={`question-${this.props.id}`} className="mx-3 my-2 py-2 px-4 border">
-                <div className="py-1">
-                    <strong>Domanda {this.props.questionNumber}</strong>
-                    <br />{difficultyText}
-                    <div dangerouslySetInnerHTML={{ __html: this.props.text }} />
-                </div>
-                <div className="px-3">
-                    {options}
-                </div>
-                <QuestionInfoPanel
-                    maxPoints={this.props.weight}
-                    score={null}
-                />
-            </div>
-        );
+    const options = shuffle(props.options.map((option, index) => (
+        <div key={`${props.id}-${index}`} className="form-check">
+            <input
+                className="form-check-input"
+                type="radio"
+                id={`ans-${props.id}-${index}`}
+                name={`q-${props.id}-${index}`} />
+            <label className="form-check-label">
+                <div dangerouslySetInnerHTML={{ __html: option }} />
+            </label>
+        </div>
+    )));
+    var difficultyText = <span className="badge bg-success">Facile</span>;
+    if (props.weight > 2) {
+        difficultyText = (props.weight > 4) ?
+            <span className="badge bg-warning">Media</span> :
+            <span className="badge bg-danger">Difficile</span>;
     }
+    return (
+
+        <div id={`question-${props.id}`} className="mx-3 my-2 py-2 px-4 border">
+            <div className="py-1">
+                <strong>Domanda {props.questionNumber}</strong>
+                <br />{difficultyText}
+                <div dangerouslySetInnerHTML={{ __html: props.text }} />
+            </div>
+            <div className="px-3">
+                {options}
+            </div>
+            <QuestionInfoPanel
+                maxPoints={props.weight}
+                score={null}
+            />
+        </div>
+    );
+
 }
 
 export default SingleQuestion;
